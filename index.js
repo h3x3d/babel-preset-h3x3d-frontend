1732f8a1fe8819d87c3c33881a9279eff21eb75b
h3x3d <h3x3d@ya.ru>
2016-10-20 00:40:02 +0300

    Updates

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 module.exports = {
   presets: [
-    [require('babel-preset-latest').default, { es2015: { modules: false } }],
-    require('babel-preset-es2016'),
-    require('babel-preset-es2017'),
-    require('babel-preset-react')
+    ['latest', { es2015: { modules: false } }],
+    'react'
   ],
   plugins: [
-    require('babel-plugin-transform-class-properties'),
-    [require('babel-plugin-transform-builtin-extend').default, { globals: ['Error'] }],
-    require('babel-plugin-transform-runtime'),
+    'transform-class-properties',
+    ['transform-builtin-extend', { globals: ['Error'] }],
+    'transform-runtime',
   ]
 };
